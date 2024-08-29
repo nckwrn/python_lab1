cb6fefe76c0ea039f777e568ddea401bb7552b75
is-student <nckwrn1@gmail.com>
2024-08-29 11:53:41 -0700

    Finished Lab 1

---
+++ lab1.py
@@ -0,0 +1,5 @@
+import math
+print (math.pi)
+print("Area of a circle with radius 5 =", math.pi * 5**2)
+print ("Volume of a sphere with radius 3 =",4/3*math.pi*3**3)
+print ("Hypotenuse of a right-angled triangle with sides 3 and 4 =",math.sqrt(3**2+4**2))
