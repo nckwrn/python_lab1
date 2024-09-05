f59438f1c0ba027c31a4ed9038bf3de1081fa143
nckwrn <nckwrn1@gmail.com>
2024-09-05 10:52:42 -0700

    Update lab1.py
    Full Lab 01

--- lab1.py
+++ lab1.py
@@ -1,5 +1,19 @@
 import math
-print (math.pi)
 print("Area of a circle with radius 5 =", math.pi * 5**2)
 print ("Volume of a sphere with radius 3 =",4/3*math.pi*3**3)
 print ("Hypotenuse of a right-angled triangle with sides 3 and 4 =",math.sqrt(3**2+4**2))
+name="Nick Wren"
+print ("Name Length =",len(name))
+print ('Name = ' "Nick", "Wren")
+upper_name = name.upper()
+lower_name = name.lower()
+print (upper_name)
+print (lower_name)
+age=29
+height=5.66666666666
+weight=150
+print ("Age variable datetype = ",type(age))
+print ("Height variable datetype = ",type(height))
+print ("Weight variable datetype = ",type(weight))
+BMI=(weight/(height*12)**2)*703
+print ("BMI = ", BMI)
